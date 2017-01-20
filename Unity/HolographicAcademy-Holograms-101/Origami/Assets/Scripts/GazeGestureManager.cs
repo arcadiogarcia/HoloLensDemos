cc9f8ac9f1752ea826ed3fc950065591d29e2656
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-01-20 10:04:18 +0100

    TechFest Demos

---
+++ Unity/HolographicAcademy-Holograms-101/Origami/Assets/Scripts/GazeGestureManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.VR.WSA.Input;
+
+public class GazeGestureManager : MonoBehaviour
+{
+    public static GazeGestureManager Instance { get; private set; }
+
+    // Represents the hologram that is currently being gazed at.
+    public GameObject FocusedObject { get; private set; }
+
+    GestureRecognizer recognizer;
+
+    // Use this for initialization
+    void Awake()
+    {
+        Instance = this;
+
+        // Set up a GestureRecognizer to detect Select gestures.
+        recognizer = new GestureRecognizer();
+        recognizer.TappedEvent += (source, tapCount, ray) =>
+        {
+            // Send an OnSelect message to the focused object and its ancestors.
+            if (FocusedObject != null && !FocusedObject.name.Contains("spatial-mapping"))
+            {
+                FocusedObject.SendMessageUpwards("OnSelect");
+            }else
+            {
+                var newBall = GameObject.Instantiate(Resources.Load("Ball"), Camera.main.transform.position, Quaternion.Euler(new Vector3(0, 0, 0))) as GameObject;
+                var rigidbody = newBall.gameObject.AddComponent<Rigidbody>();
+                rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+                var pushDirection = Camera.main.transform.forward;
+                var intensity = UnityEngine.Random.Range(12f, 15f);
+                pushDirection.x *= intensity;
+                pushDirection.y *= intensity;
+                pushDirection.z *= intensity;
+                newBall.GetComponent<Rigidbody>().AddForce(pushDirection, ForceMode.Impulse);
+            }
+        };
+        recognizer.StartCapturingGestures();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Figure out which hologram is focused this frame.
+        GameObject oldFocusObject = FocusedObject;
+
+        // Do a raycast into the world based on the user's
+        // head position and orientation.
+        var headPosition = Camera.main.transform.position;
+        var gazeDirection = Camera.main.transform.forward;
+
+        RaycastHit hitInfo;
+        if (Physics.Raycast(headPosition, gazeDirection, out hitInfo))
+        {
+            // If the raycast hit a hologram, use that as the focused object.
+            FocusedObject = hitInfo.collider.gameObject;
+        }
+        else
+        {
+            // If the raycast did not hit a hologram, clear the focused object.
+            FocusedObject = null;
+        }
+
+        // If the focused object changed this frame,
+        // start detecting fresh gestures again.
+        if (FocusedObject != oldFocusObject)
+        {
+            recognizer.CancelGestures();
+            recognizer.StartCapturingGestures();
+        }
+    }
+}
---
+++ Unity/HolographicAcademy-Holograms-101/Origami/Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class SphereCommands : MonoBehaviour
+{
+    // Called by GazeGestureManager when the user performs a Select gesture
+    void OnSelect()
+    {
+        var pushDirection = Camera.main.transform.forward;
+        var intensity = UnityEngine.Random.Range(1f, 5f);
+        pushDirection.x *= intensity;
+        pushDirection.y *= intensity;
+        pushDirection.z *= intensity;
+        this.GetComponent<Rigidbody>().AddForce(pushDirection, ForceMode.Impulse);
+    }
+}
